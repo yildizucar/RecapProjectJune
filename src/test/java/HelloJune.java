55fa8b8d67dfb7e926a80640be1ca1d7c58aafe9
yildiz ucar <yildizucr@gmail.com>
2021-07-03 23:06:56 -0400

    adding more review

--- src/test/java/HelloJune.java
+++ src/test/java/HelloJune.java
@@ -2,7 +2,7 @@ public class HelloJune {
     public static void main(String[] args) {
         System.out.println("Hello World June");
 
-        System.out.println("Recapping like day 1 June");
+        System.out.println("Recapping like day 1 June,I wanted  to reacp more june ");
     }
 
 
