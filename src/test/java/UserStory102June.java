971a8825eb914e608f8033434943d84dd6bf6d37
yildiz ucar <50220917+yildizucar@users.noreply.github.com>
2021-07-04 01:05:39 -0400

    Us102has been completed (#2)
    combined all 3 commits by squashing it

---
+++ src/test/java/UserStory102June.java
@@ -0,0 +1,8 @@
+public class UserStory102June {
+//lots of code has bee wriiten here
+    //ready to push this branch to remote
+    //amd open pull request so i can start collaborating
+     //so my team member can  comment, review and approve or request for more changes
+
+    //more changes added as requested
+}
